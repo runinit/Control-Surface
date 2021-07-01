1b690003b80c7c07b30cb0b5b5d960d725e74574
Pieter Pas <pieter.pas@outlook.com>
2021-07-02 00:28:47 +0200

    Fix test-Construction.cpp for new pin_t

--- test/MIDI_Outputs/test-Construction.cpp
+++ test/MIDI_Outputs/test-Construction.cpp
@@ -80,64 +80,64 @@ TEST(Construction, MIDIOutputs) {
         3,
         4,
     }};
-    Bank<4> bank{8};
+    Bank<4> bank {8};
 
     // CC ----------------------------------------------------------------------
-    CCButton{pin, address};
-    CCButtonLatched{pin, address};
-    CCButtonLatching{pin, address};
-    CCButtonMatrix<3, 4>{rowPins3, colPins4, addressMatrix34, cnChannel};
-    CCButtons<3>{{pin, pin, pin}, address, relAddress};
-    CCIncrementDecrementButtons{{pin, pin}, address, u8, address};
+    CCButton {pin, address};
+    CCButtonLatched {pin, address};
+    CCButtonLatching {pin, address};
+    CCButtonMatrix<3, 4> {rowPins3, colPins4, addressMatrix34, cnChannel};
+    CCButtons<3> {{pin, pin, pin}, address, relAddress};
+    CCIncrementDecrementButtons {{pin, pin}, address, u8, address};
 
-    CCPotentiometer{pin, address};
+    CCPotentiometer {pin, address};
 
-    CCRotaryEncoder{{pin, pin}, address, i8, u8};
+    CCRotaryEncoder {{u8, u8}, address, i8, u8};
 
     // Note --------------------------------------------------------------------
-    NoteButton{pin, address};
-    NoteButtonLatched{pin, address};
-    NoteButtonLatching{pin, address};
-    NoteButtonMatrix<3, 4>{rowPins3, colPins4, addressMatrix34, cnChannel};
-    NoteButtons<3>{{pin, pin, pin}, address, relAddress};
+    NoteButton {pin, address};
+    NoteButtonLatched {pin, address};
+    NoteButtonLatching {pin, address};
+    NoteButtonMatrix<3, 4> {rowPins3, colPins4, addressMatrix34, cnChannel};
+    NoteButtons<3> {{pin, pin, pin}, address, relAddress};
 
     // PB ----------------------------------------------------------------------
-    PBPotentiometer{pin, cnChannel};
+    PBPotentiometer {pin, cnChannel};
 
     // PC ----------------------------------------------------------------------
-    PCButton{pin, address};
+    PCButton {pin, address};
 
     // Bankable::CC ------------------------------------------------------------
-    Bankable::CCButton{bank, pin, address};
-    Bankable::CCButtonLatched<4>{bank, pin, address};
-    Bankable::CCButtonLatching{bank, pin, address};
-    Bankable::CCButtonMatrix<3, 4>{bank, rowPins3, colPins4, addressMatrix34,
-                                   cnChannel};
-    Bankable::CCButtons<3>{bank, {pin, pin, pin}, address, relAddress};
-    Bankable::CCIncrementDecrementButtons{
+    Bankable::CCButton {bank, pin, address};
+    Bankable::CCButtonLatched<4> {bank, pin, address};
+    Bankable::CCButtonLatching {bank, pin, address};
+    Bankable::CCButtonMatrix<3, 4> {bank, rowPins3, colPins4, addressMatrix34,
+                                    cnChannel};
+    Bankable::CCButtons<3> {bank, {pin, pin, pin}, address, relAddress};
+    Bankable::CCIncrementDecrementButtons {
         bank, {pin, pin}, address, u8, address};
 
-    Bankable::CCPotentiometer{bank, pin, address};
+    Bankable::CCPotentiometer {bank, pin, address};
 
-    Bankable::CCRotaryEncoder{bank, {pin, pin}, address, i8, u8};
+    Bankable::CCRotaryEncoder {bank, {u8, u8}, address, i8, u8};
 
     // Bankable::Note ----------------------------------------------------------
-    Bankable::NoteButton{bank, pin, address};
-    Bankable::NoteButtonLatched<4>{bank, pin, address};
-    Bankable::NoteButtonLatching{bank, pin, address};
-    Bankable::NoteButtonMatrix<3, 4>{bank, rowPins3, colPins4, addressMatrix34,
-                                     cnChannel};
-    Bankable::NoteButtons<3>{bank, {pin, pin, pin}, address, relAddress};
+    Bankable::NoteButton {bank, pin, address};
+    Bankable::NoteButtonLatched<4> {bank, pin, address};
+    Bankable::NoteButtonLatching {bank, pin, address};
+    Bankable::NoteButtonMatrix<3, 4> {bank, rowPins3, colPins4, addressMatrix34,
+                                      cnChannel};
+    Bankable::NoteButtons<3> {bank, {pin, pin, pin}, address, relAddress};
 
     // Bankable::PB ------------------------------------------------------------
-    Bankable::PBPotentiometer{bank, pin, cnChannel};
+    Bankable::PBPotentiometer {bank, pin, cnChannel};
 
     // Bankable::PC ------------------------------------------------------------
-    Bankable::PCButton{bank, pin, address};
+    Bankable::PCButton {bank, pin, address};
 
     // ManyAddresses::Note -------------------------------------------------------
-    Bankable::ManyAddresses::NoteButton<4>{bank, pin, addresses};
-    Bankable::ManyAddresses::NoteButtonMatrix<4, 3, 4>{
+    Bankable::ManyAddresses::NoteButton<4> {bank, pin, addresses};
+    Bankable::ManyAddresses::NoteButtonMatrix<4, 3, 4> {
         bank,
         rowPins3,
         colPins4,
@@ -146,48 +146,48 @@ TEST(Construction, MIDIOutputs) {
     };
 
     // ManyAddresses::CC -------------------------------------------------------
-    Bankable::ManyAddresses::CCButton<4>{bank, pin, addresses};
-    Bankable::ManyAddresses::CCButtonMatrix<4, 3, 4>{
+    Bankable::ManyAddresses::CCButton<4> {bank, pin, addresses};
+    Bankable::ManyAddresses::CCButtonMatrix<4, 3, 4> {
         bank,
         rowPins3,
         colPins4,
         {addressMatrix34, addressMatrix34, addressMatrix34, addressMatrix34},
         {cnChannel, cnChannel, cnChannel, cnChannel},
     };
-    Bankable::ManyAddresses::CCIncrementDecrementButtons<4>{
+    Bankable::ManyAddresses::CCIncrementDecrementButtons<4> {
         bank, {pin, pin}, addresses, 4, addresses};
 
-    Bankable::ManyAddresses::CCPotentiometer<4>{bank, pin, addresses};
+    Bankable::ManyAddresses::CCPotentiometer<4> {bank, pin, addresses};
 
     // ManyAddresses::PB -------------------------------------------------------
-    Bankable::ManyAddresses::PBPotentiometer<4>{
+    Bankable::ManyAddresses::PBPotentiometer<4> {
         bank, pin, {cnChannel, cnChannel, cnChannel, cnChannel}};
 
     // ManyAddresses::PC -------------------------------------------------------
-    Bankable::ManyAddresses::PCButton<4>{bank, pin, addresses};
+    Bankable::ManyAddresses::PCButton<4> {bank, pin, addresses};
 }
 
 TEST(Construction, Selectors) {
+    uint8_t u8 = 0;
     const pin_t pin = 0;
     const MIDIChannelCable cnChannel = {};
     const Wrap wrap = Wrap::NoWrap;
     Bank<4> bank;
     Bank<2> bank2;
 
-    EncoderSelector<4>{bank, {pin, pin, pin}, 4, wrap};
-    IncrementDecrementSelector<4>{bank, {pin, pin}, wrap};
-    IncrementSelector<4>{bank, pin};
-    ManyButtonsSelector<4>{bank, {pin, pin, pin, pin}};
-    ProgramChangeSelector<4>{bank, cnChannel};
-    SwitchSelector{bank2, pin};
+    EncoderSelector<4> {bank, {u8, u8, pin}, 4, wrap};
+    IncrementDecrementSelector<4> {bank, {pin, pin}, wrap};
+    IncrementSelector<4> {bank, pin};
+    ManyButtonsSelector<4> {bank, {pin, pin, pin, pin}};
+    ProgramChangeSelector<4> {bank, cnChannel};
+    SwitchSelector {bank2, pin};
 
-    EncoderSelectorLEDs<4>{
-        bank, {pin, pin, pin}, {pin, pin, pin, pin}, 4, wrap};
-    IncrementDecrementSelectorLEDs<4>{
+    EncoderSelectorLEDs<4> {bank, {u8, u8, pin}, {pin, pin, pin, pin}, 4, wrap};
+    IncrementDecrementSelectorLEDs<4> {
         bank, {pin, pin}, {pin, pin, pin, pin}, wrap};
-    IncrementSelectorLEDs<4>{bank, pin, {pin, pin, pin, pin}};
-    ManyButtonsSelectorLEDs<4>{
+    IncrementSelectorLEDs<4> {bank, pin, {pin, pin, pin, pin}};
+    ManyButtonsSelectorLEDs<4> {
         bank, {pin, pin, pin, pin}, {pin, pin, pin, pin}};
-    ProgramChangeSelectorLEDs<4>{bank, cnChannel, {pin, pin, pin, pin}};
-    SwitchSelectorLED{bank2, pin, pin};
+    ProgramChangeSelectorLEDs<4> {bank, cnChannel, {pin, pin, pin, pin}};
+    SwitchSelectorLED {bank2, pin, pin};
 }
